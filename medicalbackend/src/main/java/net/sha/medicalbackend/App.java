602393377e73e1f33dc56cb768ecb16ee811d9b2
shashi kant <shashikant.verma1@gmail.com>
2018-08-31 18:26:43 +0100

    My project setup

---
+++ medicalbackend/src/main/java/net/sha/medicalbackend/App.java
@@ -0,0 +1,13 @@
+package net.sha.medicalbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
